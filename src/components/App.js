8f7977eef28548a069abf871b2cf7a1d9e3c3d0f
Brayden Killeen <18531092@students.latrobe.edu.au>
2019-02-26 09:08:37 -0800

    Finished styling for sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import { MuiThemeProvider } from '@material-ui/core';
 import theme from './theme';
 import './App.css';
 import SideBar from './sidebar/SideBar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCode, faBook, faGraduationCap, faArchive, faCog } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCode, faBook, faGraduationCap, faArchive, faCog);
 
 class App extends Component {
   render() {
--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,24 +1,74 @@
 import React from 'react';
-import { Drawer, withStyles, List, ListItem, ListItemText, Typography } from '@material-ui/core';
-import {} from '@material-ui/icons';
+import { Drawer, withStyles, List, ListItem, ListItemText, ListItemIcon, Typography } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const drawerWidth = 200;
 
 const styles = theme => ({
   root: {
     display: 'flex'
   },
   drawer: {
-    width: 200,
+    width: drawerWidth,
     flexShrink: 0,
   },
   drawerPaper: {
-    width: 200,
-    backgroundColor: '#232e42'
+    width: drawerWidth,
+    backgroundColor: '#172B4D'
   },
   toolbar: theme.mixins.toolbar,
+  listItem: {
+    '&:hover': {
+      backgroundColor: '#42526E'
+    }
+  },
   listItemText: {
+    color: '#ffffff',
+    fontWeight: 500,
+    marginLeft: -10
+  },
+  listItemIcon: {
+    height: 20,
+    width: 20,
+  },
+  icon: {
     color: '#ffffff'
+  },
+  bottomList: {
+    height: '100%',
+    position: 'relative'
+  },
+  bottomListItem: {
+    '&:hover': {
+      backgroundColor: '#42526E'
+    },
+    position: 'absolute',
+    bottom: 0,
   }
 });
+
+const menuItems = [
+  {
+    'name': 'Study',
+    'icon': 'graduation-cap',
+    'color': '#00C7E6',
+  },
+  {
+    'name': 'Questions',
+    'icon': 'code',
+    'color': '#57D9A3',
+  },
+  {
+    'name': 'Resources',
+    'icon': 'archive',
+    'color': '#FFC400',
+  },
+  {
+    'name': 'Settings',
+    'icon': 'cog',
+    'color': '#6B778C',
+  },
+]
 
 const SideBar = (props) => {
   const { classes } = props;
@@ -28,27 +78,30 @@ const SideBar = (props) => {
       <Drawer
         className={classes.drawer}
         variant="permanent"
+        anchor="left"
         classes={{
           paper: classes.drawerPaper,
         }}
-        anchor="left"
       >
-      <div className={classes.toolbar} />
-      <List className={classes.list}>
-        {['Algorithms', 'Data Structures', 'Questions'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText
-              disableTypography
-              primary={
-                <Typography
-                  type="body2"
-                  className={classes.listItemText}
-                >
-                  {text}
-                </Typography>}
-            />
-          </ListItem>
-        ))}
+        <div className={classes.toolbar} />
+        <List className={classes.list}>
+          {menuItems.map((item, index) => (
+            <ListItem button key={index} className={classes.listItem}>
+              <ListItemIcon className={classes.listItemIcon}>
+                <FontAwesomeIcon style={{color: item['color']}} icon={item['icon']} />
+              </ListItemIcon>
+              <ListItemText
+                disableTypography
+                primary={
+                  <Typography
+                    type="body2"
+                    className={classes.listItemText}
+                  >
+                    {item['name']}
+                  </Typography>}
+              />
+            </ListItem>
+          ))}
       </List>
       </Drawer>
     </div>
